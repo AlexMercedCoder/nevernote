6f8e7b84f6df3644152fbd0ff8a14d5acd102005
Alex Merced <willcodeforfood@alexmerced.com>
2020-09-27 19:34:15 -0400

    first commit

---
+++ views/notes/new.jsx
@@ -0,0 +1,17 @@
+const React = require("react");
+const Layout = require("../layout.jsx");
+
+class Index extends React.Component {
+  render() {
+    return (
+      <Layout title="New Notes">
+        <form action="/notes/" method="post">
+          <input type="text" placeholder="write your note here" name="note" />
+          <input type="submit" value="create note" />
+        </form>
+      </Layout>
+    );
+  }
+}
+
+module.exports = Index;
